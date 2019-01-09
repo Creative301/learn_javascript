03cff0e4e6d3f8efa5425cb13066bc024512e68d
Mohammad Reza <rza9001@gmail.com>
2019-01-09 23:25:57 +0700

    Update app.js

--- Jonas/_Budgety_app/app.js
+++ Jonas/_Budgety_app/app.js
@@ -26,7 +26,31 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-  // some code later
+  // Create css class variables
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  // return supaya bisa diakses oleh function yg lain di outer scope
+  return {
+    getInput: function() {
+      // baca value yg ada di input tag
+      // Return 3 values at the same time, return an object containing these 3 as properties
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp (dari select option value di index.html)
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    // Exposing DOMstrings object into the public
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+
 })();
 
 // Connecting 2 controller
@@ -45,9 +69,25 @@ var controller = (function(budgetCtrl, UICtrl) {
 // EVENT REFERENCE: developer.mozilla.org/en-US/docs/Web/Events
 // KEYCODE REFERENCE: http://keycodes.atjayjo.com/#charcode
 var controller = (function(budgetCtrl, UICtrl) {
+  var setupEventListeners = function() {
+    // Get DOMstrings from the UI controller
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    // listen for keyboard EVENT on the document level
+    document.addEventListener('keypress', function(event) {
+      // Detect if the Enter button is pressed
+      if(event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
   var ctrlAddItem = function() {
     // 1. Get the field input data
-
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget CONTROLLER
 
     // 3. Add the item to the UI
@@ -55,17 +95,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-    console.log('it works');
-  }
+  };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-  // listen for keyboard EVENT on the document level
-  document.addEventListener('keypress', function(event) {
-    // Detect if the Enter button is pressed
-    if(event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function () {
+      // console.log('Aplication started');
+      setupEventListeners();
     }
-  });
-
+  };
 })(budgetController, UIController);
+
+// Initialize setupEventListeners function
+controller.init();
