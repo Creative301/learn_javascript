61dd4c6f430648f58e695ff43a6c2c502d54e641
Mohammad Reza <rza9001@gmail.com>
2019-01-12 15:25:07 +0700

    Update app.js

--- Jonas/_Budgety_app/app.js
+++ Jonas/_Budgety_app/app.js
@@ -20,7 +20,59 @@ var budgetController = (function() {
 
 // BUDGET CONTROLLER
 var budgetController = (function() {
-  
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+      // [1,2,4,6,8], next ID = 9
+      // ID = last ID + 1
+
+      // Membuat ID baru buat var data
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id+1;
+      } else {
+        ID = 0;
+      }
+
+      // Membuat item baru berdasarkan type 'inc' atau 'exp'
+      if(type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val)
+      }
+
+      // push (insert) data dari form input add description ke var data
+      data.allItems[type].push(newItem);
+
+      // return the new element
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
+    }
+  };
 })();
 
 
@@ -44,6 +96,29 @@ var UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+    addListItem: function (obj, type) {
+      var html, newHTML;
+      // Create HTML string with placeholder text
+      if (type === 'inc') {
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      } else if (type === 'exp') {
+      html = '<div class="item clearfix" id="expense--%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+
+      // Replace the placeholder text with some actual data
+      // obj.id ambil dari budgetCtrl Expense
+      newHTML = html.replace('%id%', obj.id);
+      newHTML = newHTML.replace('%description%', obj.description);
+      newHTML = newHTML.replace('%value%', obj.value);
+
+      // Insert the HTML into the DOM
+      // developer.mozilla.org/en-US/docs/WEB/API/Element/InsertAdjacentHTML
+
+
+
+
+
     },
     // Exposing DOMstrings object into the public
     getDOMstrings: function() {
@@ -85,10 +160,15 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
+
     // 1. Get the field input data
-    var input = UICtrl.getInput();
-    console.log(input);
+    input = UICtrl.getInput();
+    // console.log(input);
+
     // 2. Add the item to the budget CONTROLLER
+    // addItem return an object
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI
 
