d8dff47a3825b53a44af647bc272eb43fc0c06a8
creative301 <rza9001@gmail.com>
2019-01-16 22:24:52 +0000

    Update app.js

--- Jonas/_Budgety_app/app.js
+++ Jonas/_Budgety_app/app.js
@@ -31,6 +31,20 @@ var budgetController = (function() {
     this.description = description;
     this.value = value;
   };
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.allItems[type].forEach(function (cur) {
+      sum += cur.value;
+      /*
+      0
+      [200, 400, 100]
+      sum = 0 + 200
+      sum = 200 + 400
+      sum = 600 + 100 = 700
+      */
+    });
+    data.totals[type] = sum;
+  };
 
   var data = {
     allItems: {
@@ -40,7 +54,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -71,10 +87,30 @@ var budgetController = (function() {
     },
     calculateBudget: function () {
       // calculate total income and expenses
-      
+      calculateTotal('exp');
+      calculateTotal('inc');
+
       // calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
 
       // calculate the percentage of income that we spent
+      // Pakai if else supaya tidak dibagi dengan nol
+      // expense = 100 and income = 200, spent 50% = 100/200 = 0.5 * 100
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+
+    },
+    // make public
+    getBudget: function () {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
     },
     testing: function () {
       console.log(data);
@@ -92,7 +128,11 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   }
 
   // return supaya bisa diakses oleh function yg lain di outer scope
@@ -145,6 +185,19 @@ var UIController = (function() {
       // Cursor focus balik ke field pertama
       fieldsArr[0].focus();
     },
+    displayBudget: function (obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      // Setting dan menampilkan persentase (%)
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+      }
+
+    },
     // Exposing DOMstrings object into the public
     getDOMstrings: function() {
       return DOMstrings;
@@ -187,11 +240,15 @@ var controller = (function(budgetCtrl, UICtrl) {
  // Menghitung dan dan menampilkan income/expenses
   var updateBudget = function () {
     // 1. Calculate the budget
+    budgetCtrl.calculateBudget();
 
     // 2. Return the budget
+    var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
+    UICtrl.displayBudget(budget);
   };
+
   // Action when users hit the button
   var ctrlAddItem = function() {
     var input, newItem;
@@ -219,6 +276,13 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function () {
       console.log('Aplication started');
+      // Set all the value to 0
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
